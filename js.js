d985be3605ad697ff915e3d2ff2823755b525d95
Goran <gfvaskei@gmail.com>
2020-11-16 19:25:04 +0100

    Init

---
+++ js.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var ids = document.querySelectorAll("td");
+
+  var id_values = [];
+  ids.forEach(element => {
+    id_values.push(parseInt(element.attributes.item(0).value));
+  });
+
+  const today = Math.round(new Date().getTime() / 1000);
+
+  var temp = id_values.map(d => Math.abs(today - d + 360));
+  var idx = temp.indexOf(Math.min(...temp));
+
+  var current_cell = document.getElementById(id_values[idx]);
+  current_cell.classList.add("current-rare");
+
+  window.scrollTo({top: document.querySelector(".current-rare").offsetTop, left: document.querySelector(".current-rare").offsetLeft - document.querySelector(".first-column").offsetWidth, behavior: 'smooth' });
+  
+});
