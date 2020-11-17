3485eacd26fb18ce1bb435861345560cf329ccf0
Goran <gfvaskei@gmail.com>
2020-11-17 01:34:36 +0100

    Background table refresh

--- js.js
+++ js.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  var ids = document.querySelectorAll("td");
+  function refresh_table() {
+    var ids = document.querySelectorAll("td");
+  
+    var id_values = [];
+    ids.forEach(element => {
+      id_values.push(parseInt(element.attributes.item(0).value));
+    });
+  
+    const today = Math.round(new Date().getTime() / 1000);
+  
+    var temp = id_values.map(d => Math.abs(today - d + 90));
+    var idx = temp.indexOf(Math.min(...temp));
+  
+    var current_cell = document.getElementById(id_values[idx]);
+    current_cell.classList.add("current-rare");
+  
+    window.scrollTo({top: document.querySelector(".current-rare").offsetTop, left: document.querySelector(".current-rare").offsetLeft - document.querySelector(".first-column").offsetWidth, behavior: 'smooth' });
+  }
 
-  var id_values = [];
-  ids.forEach(element => {
-    id_values.push(parseInt(element.attributes.item(0).value));
-  });
+  refresh_table();
 
-  const today = Math.round(new Date().getTime() / 1000);
-
-  var temp = id_values.map(d => Math.abs(today - d + 90));
-  var idx = temp.indexOf(Math.min(...temp));
-
-  var current_cell = document.getElementById(id_values[idx]);
-  current_cell.classList.add("current-rare");
-
-  window.scrollTo({top: document.querySelector(".current-rare").offsetTop, left: document.querySelector(".current-rare").offsetLeft - document.querySelector(".first-column").offsetWidth, behavior: 'smooth' });
+  setInterval(refresh_table, 30000);
   
 });
