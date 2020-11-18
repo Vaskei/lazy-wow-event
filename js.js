6242c03397bb43b3c9bd7275cbea5267ddab55f6
Goran <gfvaskei@gmail.com>
2020-11-18 22:14:04 +0100

    Minor changes

--- js.js
+++ js.js
@@ -1,36 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   function refresh_table() {
-    console.log("Refreshing table!");
-    var ids = document.querySelectorAll("td");
+    //console.log("Refreshing table!");
+    let ids = document.querySelectorAll("td");
 
     ids.forEach(element => {
       element.classList.remove("current-rare");
     });
-    console.log("Removed classes");
+    //console.log("Removed classes");
   
-    var id_values = [];
+    let id_values = [];
     ids.forEach(element => {
       id_values.push(parseInt(element.attributes.item(0).value));
     });
   
-    const today = Math.round(new Date().getTime() / 1000);
+    let today = Math.round(new Date().getTime() / 1000);
   
-    var temp = id_values.map(d => Math.abs(today - d + 90));
-    var idx = temp.indexOf(Math.min(...temp));
-    console.log("Got current index " + idx);
+    let temp = id_values.map(d => Math.abs(today - d + 90));
+    let idx = temp.indexOf(Math.min(...temp));
+    //console.log("Got current index " + idx);
   
-    var current_cell = document.getElementById(id_values[idx]);
-    console.log("Got curent element " + current_cell);
+    let current_cell = document.getElementById(id_values[idx]);
+    //console.log("Got curent element " + current_cell);
 
     current_cell.classList.add("current-rare");
-    console.log("Added class to current element");
+    //console.log("Added class to current element");
   
     window.scrollTo({top: document.querySelector(".current-rare").offsetTop, left: document.querySelector(".current-rare").offsetLeft - document.querySelector(".first-column").offsetWidth, behavior: 'smooth' });
   }
 
   refresh_table();
 
-  setInterval(refresh_table, 5000);
+  setInterval(refresh_table, 30000);
   
 });
