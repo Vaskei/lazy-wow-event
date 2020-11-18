4ac404de28a542dad6c177875c372e633c505e9d
Goran <gfvaskei@gmail.com>
2020-11-18 21:42:45 +0100

    Added console logs

--- js.js
+++ js.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   function refresh_table() {
+    console.log("Refreshing table!");
     var ids = document.querySelectorAll("td");
 
     ids.forEach(element => {
       element.classList.remove("current-rare");
     });
+    console.log("Removed classes");
   
     var id_values = [];
     ids.forEach(element => {
@@ -16,9 +18,13 @@ document.addEventListener('DOMContentLoaded', function () {
   
     var temp = id_values.map(d => Math.abs(today - d + 90));
     var idx = temp.indexOf(Math.min(...temp));
+    console.log("Got current index " + idx);
   
     var current_cell = document.getElementById(id_values[idx]);
+    console.log("Got curent element " + current_cell);
+
     current_cell.classList.add("current-rare");
+    console.log("Added class to current element");
   
     window.scrollTo({top: document.querySelector(".current-rare").offsetTop, left: document.querySelector(".current-rare").offsetLeft - document.querySelector(".first-column").offsetWidth, behavior: 'smooth' });
   }
